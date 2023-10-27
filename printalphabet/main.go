6bf1f288e58964e89d9a27049a5f0c62f688e488
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-10-27 12:04:44 +0200

    printalphabet corrected

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -5,8 +5,8 @@ import "github.com/01-edu/z01"
 func main() {
     char := 'a'
     for i := 0; i < 26; i++ {
-        z01.PrintRune(char)
-        char++
+            z01.PrintRune(char)
+            char++
     }
     z01.PrintRune('\n')
 }
