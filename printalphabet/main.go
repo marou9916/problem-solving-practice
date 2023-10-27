7c80e237440cc1c495218ff120cc79a00a813123
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-10-27 12:32:04 +0200

    printalphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,11 +3,11 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-    char := 'a'
-    for i := 0; i < 26; i++ {
-      z01.PrintRune(char)
-      char++
-    }
-    z01.PrintRune('\n')
+
+  char:='a'
+   for i:=1; i<=26; i++{
+    z01.PrintRune(char)
+    char++
+   }
 }
 
