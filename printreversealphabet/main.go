e4aa4ae6974ec0237737ef9356200db0496f69e0
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-10-27 11:55:46 +0200

    printreversealphabet

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -2,11 +2,12 @@ package main
 
 import "github.com/01-edu/z01"
 
- func main() {
- char := 'z'
- for i := 1; i < 27; i++ {
-	z01.PrintRune(char)
-	char--
- }
+func main() {
+	char := 'z'
+	for i := 1; i < 27; i++ {
+		z01.PrintRune(char)
+		char--
+	}
 	z01.PrintRune('\n')
+
 }
