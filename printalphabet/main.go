16729d69d0df30393af44493d04c8a469fcede89
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-10-27 12:00:11 +0200

    " corrected first two exercises quest2"

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,17 +1,12 @@
 package main
 
 import "github.com/01-edu/z01"
- 
- func main(){
- char:='a'
- for i:=1; i < 27; i++ {
-  z01.PrintRune(char)
-  char++
- }
-  z01.PrintRune('\n')
 
+func main() {
+    char := 'a'
+    for i := 0; i < 26; i++ {
+        z01.PrintRune(char)
+        char++
+    }
+    z01.PrintRune('\n')
 }
-
-
-
-
--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,11 +3,10 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	char := 'z'
-	for i := 1; i < 27; i++ {
-		z01.PrintRune(char)
-		char--
-	}
-	z01.PrintRune('\n')
-
+    char := 'z'
+    for i := 0; i < 26; i++ {
+        z01.PrintRune(char)
+        char--
+    }
+    z01.PrintRune('\n')
 }
