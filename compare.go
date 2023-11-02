befd9383cfec32eb86dfaa8ce3cc0b4b20019911
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-11-02 22:04:17 +0100

    compare.go

---
+++ compare.go
@@ -0,0 +1,23 @@
+package piscine
+
+func Compare(a string, b string) int {
+	if len(a) < len(b) {
+		for i := 0; i <= len(a); i++ {
+			if a[i] == b[i] {
+				return -1
+			}
+		}
+	} else if len(a) == len(b) {
+		for i := 0; i <= len(a); i++ {
+			if a[i] == b[i] {
+				return 0
+			}
+		}
+	} else if len(a) > len(b) {
+		return -1
+	}
+	return 0
+
+}
+
+
