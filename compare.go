3925e7c84a0dcc6ac6409c7dceaab7f23fc543d0
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-11-02 22:43:00 +0100

    compare.go mn2

--- compare.go
+++ compare.go
@@ -13,13 +13,8 @@ func Compare(a string, b string) int {
 			return 1
 		}
 	}
-
-	if len(a) == len(b) {
-		for i := 0; i <= len(a); i++ {
-			if a[i] == b[i] {
-				return 0
-			}
-		}
+	if len(a) < len(b) {
+		return -1
 	} else if len(a) > len(b) {
 		return 1
 	}
