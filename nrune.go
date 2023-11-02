c94ba56989269b9f958928d99b76984ad72ae03f
marou9916 <oumar.ndiaye447@gmail.com>
2023-11-02 09:07:31 +0100

    nrune modif2

--- nrune.go
+++ nrune.go
@@ -1,12 +1,12 @@
 package piscine
 
 func NRune(s string, n int) rune {
-s1 := []rune(s)
-if n < 0 || n > len(s)-1 {
-	return 0
-} else {
-	return s1[n]
-}
+	s1 := []rune(s)
+	if n < 0 || n > len(s)-1 {
+		return 0
+	} else {
+		return s1[n]
+	}
 }
 
 
