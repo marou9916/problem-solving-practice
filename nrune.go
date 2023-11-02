c84de5749cb3474cccfd91e55d3517fc91c0f580
marou9916 <oumar.ndiaye447@gmail.com>
2023-11-02 09:47:16 +0100

    nrune modif 8

--- nrune.go
+++ nrune.go
@@ -5,6 +5,12 @@ func NRune(s string, n int) rune {
 	if n < 0 || n > len(s)-1 {
 		return 0
 	} else {
-		return s1[n-1]
+		for index := range s {
+			if index == n {
+				index = n-1
+			}
+			break
+		}
+		return s1[index]
 	}
 }
