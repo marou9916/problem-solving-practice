d8808817e26c4bb6cc1613437e1e751a6300ee05
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-11-02 17:24:47 +0100

    printprogramname/main.go m2

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -9,7 +9,8 @@ import (
 func main() {
 	x := os.Args[0]
 	nameProgram := []rune(x)
-	for i := 2; i <= len(x); i++ {
+	for i := 2; i < len(x); i++ {
 		z01.PrintRune(nameProgram[i])
 	}
+	z01.PrintRune('\n')
 }
