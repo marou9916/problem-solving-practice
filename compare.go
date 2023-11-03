02ce94af2b5af5c768ea7233edf6bc7e77c35388
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-11-03 10:35:06 +0100

    compare.go

--- compare.go
+++ compare.go
@@ -1,22 +1,12 @@
 package piscine
 
 func Compare(a string, b string) int {
-	minimaLength := len(a)
-	if len(b) < minimaLength {
-		minimaLength = len(b)
-	}
-
-	for i := 0; i < minimaLength; i++ {
-		if a[i] < b[i] {
-			return -1
-		} else {
-			return 1
-		}
-	}
-	if len(a) < len(b) {
-		return -1
-	} else if len(a) > len(b) {
+	if a == b {
+		return 0
+	} else if a > b {
 		return 1
+	} else {
+		return -1
 	}
-	return 0
 }
+
