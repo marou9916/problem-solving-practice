f58b229412fc6b6c6f442c5ebc135c8f4497fc83
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-11-03 11:47:24 +0100

    alphacount m6

--- alphacount.go
+++ alphacount.go
@@ -1,7 +1,7 @@
 package piscine
 
 func AlphaCount(s string) int {
-	count := 1
+	count := 0
 	s1 := []rune(s)
 	for i := 0; i <= len(s)-1; i++ {
 		if s1[i] >= 'a' && s1[i] <= 'z' || s1[i] >= 'A' && s1[i] <= 'Z' || s1[i] < '0' && s1[i] > '9' {
