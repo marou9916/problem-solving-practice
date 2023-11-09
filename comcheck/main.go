d2f389c8f3739aabf5ee5afdbc4015e14ff3f897
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-11-09 14:31:59 +0100

    !

--- comcheck/main.go
+++ comcheck/main.go
@@ -9,7 +9,7 @@ func main() {
 	args := os.Args[1:]
 	for _, i := range args {
 		if i == "01" || i == "galaxy" || i == "galaxy 01" {
-			fmt.Println("Alert!")
+			fmt.Println("Alert!!!")
 			break
 		}
 	}
