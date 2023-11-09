9d5af6da3fd45aa34cdd719fcafb2afa24038364
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-11-09 18:00:24 +0100

    enigma m0

--- enigma.go
+++ enigma.go
@@ -1,12 +1,11 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-   	    l := *******c
-   	    m := ****d
-    	n := *b
-    	*******c = ***a
-  	  ****d = l
-   	  *b = m
-      ***a = n
+	l := *******c
+	m := ****d
+	n := *b
+	*******c = ***a
+	****d = l
+	*b = m
+	***a = n
 }
-
