bac51ded88243cf3eec7df5932e2ec68c109e8e1
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-11-09 21:23:37 +0100

    fixthemain/main.go

---
+++ fixthemain/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type Door struct {
+	state string
+}
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func CloseDoor(ptrDoor *Door) bool {
+	PrintStr("Door Closing...")
+	ptrDoor.state = "CLOSE"
+	return true
+}
+func OpenDoor(ptrDoor *Door) bool {
+	PrintStr("Door Opening...")
+	ptrDoor.state = "OPEN"
+	return true
+}
+
+func IsDoorOpen(ptrDoor *Door) bool {
+	PrintStr("is the Door opened ?")
+	if ptrDoor.state == "OPEN" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?")
+	if ptrDoor.state == "CLOSE" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+	if door.state == "OPEN"{
+		CloseDoor(door)
+	}
+}
+
+    
