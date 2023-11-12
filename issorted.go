a6efd38305f9d8a750c913259c122cf45d4e13fb
Oumar NDIAYE <oumar.ndiaye447@gmail.com>
2023-11-12 21:51:44 +0100

    is sorted 2

--- issorted.go
+++ issorted.go
@@ -1,14 +1,14 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, tab []int) bool {
-	if tab[0] < tab[1] {
+	if f(tab[0], tab[1]) <= 0 {
 		for i := 0; i < len(tab)-1; i++ {
 			if f(tab[i], tab[i+1]) > 0 {
 				return false
 			}
 		}
 	}
-	if tab[0] > tab[1] {
+	if f(tab[0], tab[1]) >= 0 {
 		for i := 0; i < len(tab)-1; i++ {
 			if f(tab[i], tab[i+1]) < 0 {
 				return false
