e3304a9d4792c22303de638d3ef5f1f715e56248
oundiaye <oumar.ndiaye447@gmail.com>
2023-12-20 13:49:30 +0100

    ispowerof2 and strlen

---
+++ ispowerof2.go
@@ -0,0 +1,23 @@
+package piscine
+
+func IsPowerOf2(number int) bool {
+	const div = 2
+	var numberAfterDiv []int
+	numberAfterDiv = append(numberAfterDiv, number)
+	if number == 1 {
+		return true
+	} else if number == 0 || number != 1 && number%div != 0 {
+		return false
+	} else if number%div == 0 {
+		for number != 0 {
+			number = number / div
+			numberAfterDiv = append(numberAfterDiv, number)
+		}
+		for i := 1; i <= len(numberAfterDiv)-3; i++ {
+			if numberAfterDiv[i]%div != 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
--- strlen.go
+++ strlen.go
@@ -2,9 +2,22 @@ package piscine
 
 func StrLen(s string) int {
 	s1 := []rune(s)
-	runeCounter := 0
+	var runesCounter int
 	for i := 0; i <= len(s1)-1; i++ {
-		runeCounter++
+		runesCounter++
 	}
-	return runeCounter
+	return runesCounter
+}
+
+func IntToRune(the_number int) []rune {
+	div := 10
+	var reste int
+	var digits_of_the_number []rune
+	for the_number != 0 {
+		reste = the_number % div
+		digits_of_the_number = append(digits_of_the_number, rune(reste+'0'))
+		the_number = the_number / div
+	}
+	return digits_of_the_number
+
 }
