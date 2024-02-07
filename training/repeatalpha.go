0c202d36e44aa94227cabeebcabb0a06af93efae
oundiaye <oumar.ndiaye447@gmail.com>
2024-02-07 11:10:19 +0100

    repeatalpha.go

--- training/repeatalpha.go
+++ training/repeatalpha.go
@@ -2,24 +2,27 @@ package training
 
 func RepeatAlpha(s string) string {
 	var result string //la string à retourner
-	var index int
+	var index int     //l'indice de la lettre dans l'alphabet
+
 	//Convertir la string en runes
 	s1 := []rune(s)
 
-	//Parcourir la string
+	//Parcourir les runes
 	for i := 0; i <= len(s1)-1; i++ {
-		if (s1[i] >= 'A' && s1[i] <= 'Z') || (s1[i] >= 'a' && s1[i] <= 'z') { //Si on tombe sur une lettre
+		if (s1[i] >= 'A' && s1[i] <= 'Z') || (s1[i] >= 'a' && s1[i] <= 'z') { //Si l'élément est une lettre
 			//Récupérer l'index de la lettre dans l'alphabet
 			index = AlphaIndex(s1[i])
 
-			//Concaténer la lettre dans la variable autant de fois que son index
+			//Concaténer la lettre dans la variable à retourner autant de fois que son index
 			for j := 1; j <= index; j++ {
-				result += string(s1[i]) //Répéter la lettre dans la string autant de fois que son rang dans l'alphabet
+				result += string(s1[i]) //Répéter la lettre dans la string autant de fois que son index dans l'alphabet
 			}
-		} else {
-			result += string(s1[i])
+		} else { //Sinon (si l'élément n'est pas une lettre)
+			result += string(s1[i]) //Ajouter l'élément sans le répéter
 		}
 	}
+
+	//Retourner le résultat
 	return result
 }
 
