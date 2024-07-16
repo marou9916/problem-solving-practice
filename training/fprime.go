add90ac8eb8407a0f28a735191d1ae9dea9803f5
oundiaye <oumar.ndiaye447@gmail.com>
2024-07-16 08:20:44 +0200

    "optimal solution after changing the algorithm to get the prime factors"

--- training/fprime.go
+++ training/fprime.go
@@ -19,20 +19,24 @@ func Fprime(tab []int) {
 func GetPrimeFactors(number int) []int {
 	primeFactors := []int{}
 
-	for i := 2; i <= number; i++ {
-		if IsPrime2(i) && number%i == 0 {
+	// Diviser par 2 jusqu'à ce que ce ne soit plus divisible
+	for number%2 == 0 {
+		primeFactors = append(primeFactors, 2)
+		number /= 2
+	}
+
+	// Diviser par les nombres impairs de 3 à sqrt(number)
+	for i := 3; i*i <= number; i += 2 {
+		for number%i == 0 {
 			primeFactors = append(primeFactors, i)
+			number /= i
 		}
+	}
+
+	// Si le nombre restant est un nombre premier supérieur à 2
+	if number > 2 {
+		primeFactors = append(primeFactors, number)
 	}
 
 	return primeFactors
-}
-
-func IsPrime2(number int) bool {
-	for i := 2; i*i <= number; i++ {
-		if number%i == 0 {
-			return false
-		}
-	}
-	return true
 }
