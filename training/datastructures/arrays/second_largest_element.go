093e6714f1d7a7e05b1aff25a2402c398e694f77
oundiaye <oumar.ndiaye447@gmail.com>
2024-10-26 09:51:38 +0200

    "more efficient solution"

--- training/datastructures/arrays/second_largest_element.go
+++ training/datastructures/arrays/second_largest_element.go
@@ -1,29 +1,16 @@
 package arrays
 
 func SecondLargestElement(arr []int) int {
-
-	n := len(arr)
-
-	if n < 2 {
-		return -1
-	}
-
-	for i := 0; i < n; i++ {
-		for j := 0; j < n-i-1; n++ {
-			if arr[j] < arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-			}
-		}   
-	}
-	
-	largest := arr[0]
+	largest, secondLargest := -1, -1
 
 	for _, element := range arr {
-		if element < largest {
-			return element
+		if element > largest {
+			secondLargest = largest
+			largest = element
+ 		} else if element < largest && element > secondLargest {
+			secondLargest = element
 		}
 	}
 
-
-	return -1
+	return secondLargest
 }
