d371171caa6784b7807e53b1898234da9f1c3d92
oundiaye <oumar.ndiaye447@gmail.com>
2024-10-28 08:45:27 +0100

    "a better approach "

--- training/datastructures/arrays/three_largest_distinct.go
+++ training/datastructures/arrays/three_largest_distinct.go
@@ -1,37 +1,31 @@
 package arrays
 
 func FindThreeLargestDistinctElements(arr []int) []int {
-	if len(arr) >= 0 && len(arr) < 3 { //si l'array contient zéro, un, ou deux éléments alors retourner l'array
+	firstLargest, secondLargest, thirdLargest := -1<<31, -1<<31, -1<<31
+
+	if len(arr) >= 0 && len(arr) < 3 {
 		return arr
 	}
 
-	//Trouver le premier élément
-	firstLargest := LargestElement(arr)
-
-	//Trouver le second élément
-	secondLargest := SecondLargestElement(arr)
-	if secondLargest == -1 { //s'il n y a pas de second élément distinct plus grand
-		return []int{firstLargest} //renvoyer une array contenant uniquement le plus grand élément distinct
-	}
-
-	//Trouver le troisième élément
-	thirdLargest := ThirdLargestElement(firstLargest, secondLargest, arr)
-	if thirdLargest == -1 { //s'il n y a pas de troisième élément distinct plus grand
-		return []int{firstLargest, secondLargest} //renvoyer une array contenant les deux plus grands éléments distincts
-	}
-
-	return []int{firstLargest, secondLargest, thirdLargest}
-}
-
-func ThirdLargestElement(firstLargest, secondLargest int, arr []int) int {
-	thirdLargest := -1
-
-	//Parcourir l'array
 	for _, element := range arr {
-		if element > thirdLargest && element != firstLargest && element != secondLargest {
+		if element > firstLargest {
+			thirdLargest = secondLargest
+			secondLargest = firstLargest
+			firstLargest = element
+		} else if element < firstLargest && element > secondLargest {
+			thirdLargest = secondLargest
+			secondLargest = element
+		} else if element < secondLargest && element > thirdLargest {
 			thirdLargest = element
 		}
 	}
 
-	return thirdLargest
+	if thirdLargest == -1<<31 {
+		if secondLargest == -1<<31 {
+			return []int{firstLargest}
+		}
+		return []int{firstLargest, secondLargest}
+	}
+
+	return []int{firstLargest, secondLargest, thirdLargest}
 }
