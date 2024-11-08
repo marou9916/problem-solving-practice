356349d3e9996eead017de1f6f10379400e58bc4
oundiaye <oumar.ndiaye447@gmail.com>
2024-11-08 11:09:17 +0100

    "bingo : better approach"

--- training/datastructures/arrays/bingo.go
+++ training/datastructures/arrays/bingo.go
@@ -1,34 +1,29 @@
 package arrays
 
-import (
-	"time"
+func Bingo(arr [25]int, randomNumber int) bool {
+	for i := 0; i < 5; i++ {
+		counter := 0
 
-	"golang.org/x/exp/rand"
-)
-
-func Bingo(arr [5][5]int) bool {
-	//Tirer un numéro aléatoire
-	rand.Seed(uint64(time.Now().UnixNano())) //Nécessaire pour générer un numéro différent à chaque exécution du programme
-	randomNumber := rand.Intn(100)
-
-	//Parcourir la grille par colonnes
-	counter := 0 //compteur de jetons
-
-	for column := 0; column < 5; column++ {
-		for line := 0; line < 5; line++ {
-			//Vérifier  si la valeur de la cellule est égale à celle du nombre aléatoire
-			if arr[line][column] == randomNumber {
-				arr[line][column] = -1
+		for j := 0; j < 5; j++ {
+			if arr[i*5+j] == randomNumber {
 				counter++
-				if counter == 5 { //Si la colonne entière est marquée
-					return true
-				}
-			} else {
-				if column == 4 { //Si la dernière colonne est atteinte
-					return false
-				}
-				continue //Si ce n'est pas la dernière colonne, on  passe à la suivante
 			}
+		}
+		if counter == 5 {
+			return true
+		}
+	}
+
+	for i := 0; i < 5; i++ {
+		counter := 0
+
+		for j := 0; j < 5; j++ {
+			if arr[j*5+i] == randomNumber {
+				counter++
+			}
+		}
+		if counter == 5 {
+			return true
 		}
 	}
 	return false
