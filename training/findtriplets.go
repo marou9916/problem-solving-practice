2b68dadada29f6d149e9b392a997d9b7c37d6185
oundiaye <oumar.ndiaye447@gmail.com>
2025-01-26 11:25:45 +0100

    "different approach, more efficient"

--- training/findtriplets.go
+++ training/findtriplets.go
@@ -1,5 +1,6 @@
 package training
 
+import "sort"
 
 func FindTriplets(arr []int) [][]int {
 	var result [][]int
@@ -17,4 +18,40 @@ func FindTriplets(arr []int) [][]int {
 	return result
 }
 
+func FindTriplet(arr []int) [][]int {
+	var result [][]int
 
+	// Trier le tableau d'abord
+	sort.Ints(arr)
+
+	for i := 0; i < len(arr)-2; i++ {
+		// Éviter les doublons pour i
+		if i > 0 && arr[i] == arr[i-1] {
+			continue
+		}
+
+		left, right := i+1, len(arr)-1
+		for left < right {
+			sum := arr[i] + arr[left] + arr[right]
+			if sum == 0 {
+				result = append(result, []int{i, left, right})
+
+				// Éviter les doublons pour left et right
+				for left < right && arr[left] == arr[left+1] {
+					left++
+				}
+				for left < right && arr[right] == arr[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+
+	return result
+}
