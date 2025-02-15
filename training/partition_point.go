ec204d5ff2ebd964e1314782b05bef6974291ac8
oundiaye <oumar.ndiaye447@gmail.com>
2025-02-15 08:46:33 +0100

    "another better approach with both less space and time complexity "

--- training/partition_point.go
+++ training/partition_point.go
@@ -1,40 +1,32 @@
 package training
 
-//Now, i'm not gonna calculate the leftMax and the rightMin for each element at each time. Instead, i calculate all of them once
-//in order to reduce complexity and improve efficiency
+//This time, i'm gonna calculate all the right minimum, store them into a table, then change the left maximum dynamically
 func PartitionPoint(arr []int) int {
 	n := len(arr)
-
 	if n < 3 {
 		return -1
 	}
-	
-	leftMax := make([]int, n) 
-	leftMax[0] = arr[0]
 
 	minRight := make([]int, n)
-	minRight[n-1] = arr[n-1]
-
-	for i := 1; i <= n - 2; i++ {
-		if arr[i] > leftMax[i-1] {
-			leftMax = append(leftMax, arr[i])
+	minRight[n - 2] = arr[n - 2] 
+	
+	for i := n-3; i >= 0; i-- {
+		if minRight[i+1] < arr[i+1] {
+			minRight[i] = minRight[i+1]
 		} else {
-			leftMax = append(leftMax, leftMax[i-1])
+			minRight[i] = arr[i+1]
 		}
 	}
 
-	for i := n-2; i >= 1; i-- {
-		if arr[i] < minRight[i+1] {
-			minRight = append(minRight, arr[i])
-		} else {
-			minRight = append(minRight, minRight[i+1])
-		}
-	}
+	maxLeft := arr[0]
 
-	for i := 1; i <= n-2; i++ {
-		if arr[i] > leftMax[i-1] && arr[i] < minRight[i+1] {
-			return i
+	for j := 1; j <= n - 2; j++ {
+		if arr[j] > maxLeft && arr[j] < minRight[j] {
+			return j
 		}
+		if arr[j] > maxLeft {
+			maxLeft = arr[j]
+		} 
 	}
 
 	return -1
